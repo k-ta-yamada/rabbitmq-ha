dd5a82f2fc3487a86ecec9ac3baa4e26e718f474
k-ta-yamada <key.luvless@gmail.com>
2017-11-09 16:58:14 +0900

    [mod] cookbooks/keepalived

--- cookbooks/keepalived/conf.rb
+++ cookbooks/keepalived/conf.rb
@@ -3,20 +3,13 @@ GLOBAL_DEFS     = KEEPALIVED_CONF[:global_defs]
 VRRP_SCRIPT     = KEEPALIVED_CONF[:vrrp_script]
 VRRP_INSTANCE   = KEEPALIVED_CONF[:vrrp_instance]
 
-HOSTS_ENTRIES = node[:hosts][:frontend]
-# HOSTS_ENTRIES = node[:hosts][:backend]
+# UNICAST_PEER = node[:hosts][:frontend].map { |h| h["ip"] }
+UNICAST_PEER = node[:hosts][:backend].map { |h| h["ip"] }
 
 template "/etc/keepalived/keepalived.conf" do
-  hostname = run_command("hostname").stdout.chomp
-  unicast_src_ip = HOSTS_ENTRIES.select { |h| h["host"] == hostname }.first["ip"]
-  unicast_peer   = HOSTS_ENTRIES.reject { |h| h["host"] == hostname }.map { |h| h["ip"] }
-  puts "hostname       = [#{hostname}]"
-  puts "unicast_src_ip = [#{unicast_src_ip}]"
-  puts "unicast_peer   = [#{unicast_peer}]"
-  variables global_defs:    GLOBAL_DEFS,
-            vrrp_script:    VRRP_SCRIPT,
-            vrrp_instance:  VRRP_INSTANCE,
-            unicast_src_ip: unicast_src_ip,
-            unicast_peer:   unicast_peer
+  variables global_defs:   GLOBAL_DEFS,
+            vrrp_script:   VRRP_SCRIPT,
+            vrrp_instance: VRRP_INSTANCE,
+            unicast_peer:  UNICAST_PEER
   only_if "test -d /etc/keepalived"
 end
