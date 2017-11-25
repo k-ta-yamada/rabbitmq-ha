550a6a3bf8cc7b7f9926cbb3cc566e4ec2fec5af
k-ta-yamada <key.luvless@gmail.com>
2017-11-25 17:11:45 +0900

    [mod] cookbooks/haproxy unicast_peer change frontend

--- cookbooks/keepalived/conf.rb
+++ cookbooks/keepalived/conf.rb
@@ -3,8 +3,8 @@ GLOBAL_DEFS     = KEEPALIVED_CONF[:global_defs]
 VRRP_SCRIPT     = KEEPALIVED_CONF[:vrrp_script]
 VRRP_INSTANCE   = KEEPALIVED_CONF[:vrrp_instance]
 
-# UNICAST_PEER = node[:hosts][:frontend].map { |h| h["ip"] }
-UNICAST_PEER = node[:hosts][:backend].map { |h| h["ip"] }
+UNICAST_PEER = node[:hosts][:frontend].map { |h| h["ip"] }
+# UNICAST_PEER = node[:hosts][:backend].map { |h| h["ip"] }
 
 template "/etc/keepalived/keepalived.conf" do
   variables global_defs:   GLOBAL_DEFS,
