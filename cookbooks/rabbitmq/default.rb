5a3d0e2a381229454f35a2b57fb27bc1f9179821
k-ta-yamada <key.luvless@gmail.com>
2017-11-25 16:47:29 +0900

    [mod] cookbooks/rabbitmq change install epel-release

--- cookbooks/rabbitmq/default.rb
+++ cookbooks/rabbitmq/default.rb
@@ -1,4 +1,8 @@
-package 'epel-release'
+# package 'epel-release'
+execute "wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -P /usr/src/ -nc"
+execute "rpm -ivh /usr/src/epel-release-latest-7.noarch.rpm" do
+  not_if "yum list installed | grep epel-release"
+end
 
 node[:rabbitmq][:pkg].each do |pkg|
   package pkg
